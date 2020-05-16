4de617551da964b46c67d62e8f33cbfbe68dac25
satyam52 <satyam0350@gmail.com>
2020-05-16 21:44:57 +0530

    removed duplicates from  execption.py

--- app/api/helpers/exceptions.py
+++ app/api/helpers/exceptions.py
@@ -1,16 +1,7 @@
 from .errors import ErrorResponse
 
 
-# TODO(Areeb): Remove these duplicate errors
 
-
-class UnprocessableEntity(ErrorResponse):
-    """
-    Default class for 422 Error
-    """
-
-    title = "Unprocessable Entity"
-    status = 422
 
 
 class ConflictException(ErrorResponse):
@@ -22,13 +13,7 @@ class ConflictException(ErrorResponse):
     status = 409
 
 
-class ForbiddenException(ErrorResponse):
-    """
-    Default class for 403 Error
-    """
 
-    title = 'Access Forbidden'
-    status = 403
 
 
 class MethodNotAllowed(ErrorResponse):
